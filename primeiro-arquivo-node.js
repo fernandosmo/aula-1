25c1d8278c3c066f6e18ef09b66956c895cac61e
Fernando <fernandosmo@icloud.com>
2023-03-21 08:50:14 -0300

    first commit

---
+++ primeiro-arquivo-node.js
@@ -0,0 +1,10 @@
+console.log("Meu primeiro arquivo javascript no node!");
+
+const num1 = 2
+const num2 = 2
+
+const soma = () => {
+  return num1 + num2
+}
+
+console.log(`o resultado da soma é ${soma()}`);
